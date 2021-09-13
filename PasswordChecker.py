8980c72a722237449c97b77e80f6b4cb074dd8c8
Mike <miket3c@gmail.com>
2021-09-12 21:57:39 -0500

    Add files via upload

---
+++ PasswordChecker.py
@@ -0,0 +1,37 @@
+import PySimpleGUI as sg
+
+sg.theme('DarkGrey 13')
+
+layout = [[sg.Text('Please enter your password:'), sg.Text(size=(18,1), key='-OUTPUT-')],
+          [sg.Input(key='-IN-')],
+          [sg.Button('Submit'), sg.Button('Exit')]]
+
+window = sg.Window('Enter Password', layout, size=(400,100))
+pass_test = 0
+while True:  
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == 'Submit':
+        window['-OUTPUT-'].update(values['-IN-'])
+        if values["-IN-"] == "password":
+            window["-OUTPUT-"].update("Success!")
+            pass_test = 1
+            break
+        else: 
+            window["-OUTPUT-"].update("Incorrect. Try again.")
+
+window.close()
+
+layout2 = [[sg.Text("You successfully entered your password!")], [sg.Button("OK")]]
+
+if pass_test == 1:
+    window2 = sg.Window("Success!", layout2)
+
+    while True:
+        event, values = window2.read()
+        if event == "OK" or event == sg.WIN_CLOSED:
+            break
+
+    window2.close()
